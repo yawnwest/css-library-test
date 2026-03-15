00d282e8302b6a60c1ee89b0ca20ca80dd943bed
yawn west <yawn.west@posteo.eu>
2026-03-15 21:57:59 +0100

    Feature/ci (#5)
    * added basic ci
    
    * docs: updated README.md

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,27 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
-export default defineConfig(({ command }) => {
-  if (command === 'serve') {
-    return {
-      root: 'playground',
-    }
-  }
-
-  return {
-    build: {
-      lib: {
-        entry: resolve(__dirname, 'src/index.ts'),
-        name: 'YawnwestCssProjectTest',
-        fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`,
-        formats: ['es', 'cjs'],
-      },
-      sourcemap: true,
-      cssCodeSplit: false,
-      rollupOptions: {
-        output: {
-          assetFileNames: () => 'style.css',
-        },
+export default defineConfig({
+  root: 'playground',
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'YawnwestCssLibraryTest',
+      fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`,
+      formats: ['es', 'cjs'],
+    },
+    sourcemap: true,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        assetFileNames: () => 'style.css',
       },
     },
-  }
+  },
 })
