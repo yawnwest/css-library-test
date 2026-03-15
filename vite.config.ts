d76c58048fa1dd10fd06e7adf1c60414da61ca41
yawn west <yawn.west@posteo.eu>
2026-03-15 21:28:26 +0100

    chore: added eslint + prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,28 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig(({ command }) => {
-  if (command === "serve") {
+  if (command === 'serve') {
     return {
-      root: "playground",
-    };
+      root: 'playground',
+    }
   }
 
   return {
     build: {
       lib: {
-        entry: resolve(__dirname, "src/index.ts"),
-        name: "YawnwestCssProjectTest",
-        fileName: (format) => `index.${format === "es" ? "js" : "cjs"}`,
-        formats: ["es", "cjs"],
+        entry: resolve(__dirname, 'src/index.ts'),
+        name: 'YawnwestCssProjectTest',
+        fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`,
+        formats: ['es', 'cjs'],
       },
       sourcemap: true,
       cssCodeSplit: false,
       rollupOptions: {
         output: {
-          assetFileNames: () => "style.css",
+          assetFileNames: () => 'style.css',
         },
       },
     },
-  };
-});
+  }
+})
