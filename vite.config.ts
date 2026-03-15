db6ac975875e266baa83d9db1c4d8c85aa00d7a5
yawn west <yawn.west@posteo.eu>
2026-03-15 19:21:31 +0100

    chore: added playground (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,28 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/index.ts"),
-      name: "YawnwestCssProjectTest",
-      fileName: (format) => `index.${format === "es" ? "js" : "cjs"}`,
-      formats: ["es", "cjs"],
-    },
-    sourcemap: true,
-    cssCodeSplit: false,
-    rollupOptions: {
-      output: {
-        assetFileNames: () => "style.css",
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      root: "playground",
+    };
+  }
+
+  return {
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/index.ts"),
+        name: "YawnwestCssProjectTest",
+        fileName: (format) => `index.${format === "es" ? "js" : "cjs"}`,
+        formats: ["es", "cjs"],
+      },
+      sourcemap: true,
+      cssCodeSplit: false,
+      rollupOptions: {
+        output: {
+          assetFileNames: () => "style.css",
+        },
       },
     },
-  },
+  };
 });
